cfb65a9ab55d5ed3ed29ece2bcb8f5b4a855d6d0
lilizhaolilizhao <lilizhaolilizhao@163.com>
2019-02-20 17:41:09 +0800

    完成第一个demo

--- java-agent/src/main/java/com/github/llz/Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.llz;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("This is a test!");
-    }
-}
---
+++ java-agent/src/main/java/com/github/llz/agent/App.java
@@ -0,0 +1,11 @@
+package com.github.llz.agent;
+
+public class App {
+    public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
