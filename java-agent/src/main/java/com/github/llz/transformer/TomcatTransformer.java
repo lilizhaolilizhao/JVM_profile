cf37b4d3c61fa6df703bbef9d1e866cc99f1f7c2
lilizhaolilizhao <lilizhaolilizhao@163.com>
2019-02-25 14:40:55 +0800

    插码多个类和方法

--- java-agent/src/main/java/com/github/llz/transformer/TomcatTransformer.java
+++ java-agent/src/main/java/com/github/llz/transformer/TomcatTransformer.java
@@ -13,19 +13,58 @@ public class TomcatTransformer extends GeneralMethodAdapter {
     }
 
     @Override
-    public  MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+    public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
         MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
 
-        if (!isInterface && mv != null) {
-            MethodVisitor at = addStandardHostValue(access, name, descriptor, mv);
-            if (at != null) return at;
-        }
+        MethodVisitor at;
+        at = addStandardHostValue(access, name, descriptor, mv);
+        if (at != null) mv = at;
+
+        at = addStandardService(access, name, descriptor, mv);
+        if (at != null) mv = at;
 
         return mv;
     }
 
+    /**
+     * 添加 StandardService
+     *
+     * @param access
+     * @param name
+     * @param descriptor
+     * @param mv
+     * @return
+     */
+    private MethodVisitor addStandardService(int access, String name, String descriptor, MethodVisitor mv) {
+        if (!isInterface && mv != null
+                && JVMUtils.javaNameToJvmName("org.apache.catalina.core.StandardService").equals(owner)
+                && ("startInternal").equals(name)
+                && descriptor.equals(JVMUtils.javaTypeToJvmSignature(new String[]{}, "void"))) {
+
+            methodName = name;
+            AddTimerMethodAdapter at = new AddTimerMethodAdapter(mv);
+
+            at.aa = new AnalyzerAdapter(owner, access, name, descriptor, at);
+            at.lvs = new LocalVariablesSorter(access, descriptor, at.aa);
+
+            return at.lvs;
+        }
+        return mv;
+    }
+
+    /**
+     * 添加 StandardHostValve
+     *
+     * @param access
+     * @param name
+     * @param descriptor
+     * @param mv
+     * @return
+     */
     private MethodVisitor addStandardHostValue(int access, String name, String descriptor, MethodVisitor mv) {
-        if (JVMUtils.javaNameToJvmName("org.apache.catalina.core.StandardHostValve").equals(owner) && ("invoke").equals(name)
+        if (!isInterface && mv != null
+                && JVMUtils.javaNameToJvmName("org.apache.catalina.core.StandardHostValve").equals(owner)
+                && ("invoke").equals(name)
                 && descriptor.equals(JVMUtils.javaTypeToJvmSignature(new String[]{"org.apache.catalina.connector.Request", "org.apache.catalina.connector.Response"}, "void"))) {
             methodName = name;
             AddTimerMethodAdapter at = new AddTimerMethodAdapter(mv);
@@ -35,6 +74,6 @@ public class TomcatTransformer extends GeneralMethodAdapter {
 
             return at.lvs;
         }
-        return null;
+        return mv;
     }
 }
