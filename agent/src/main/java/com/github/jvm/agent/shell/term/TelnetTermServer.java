7a8fdcddd268eb064575f80cfd316bb9e87763e1
lilizhaolilizhao <lilizhaolilizhao@163.com>
2019-03-17 22:04:37 +0800

    初步引入telnet服务库

---
+++ agent/src/main/java/com/github/jvm/agent/shell/term/TelnetTermServer.java
@@ -0,0 +1,22 @@
+package com.github.jvm.agent.shell.term;
+
+import io.termd.core.telnet.netty.NettyTelnetTtyBootstrap;
+
+/**
+ * 建立telnet服务端
+ */
+public class TelnetTermServer {
+    private NettyTelnetTtyBootstrap bootstrap;
+    private String hostIp;
+    private int port;
+    private long connectionTimeout;
+
+    public TelnetTermServer(String hostIp, int port, long connectionTimeout) {
+        this.hostIp = hostIp;
+        this.port = port;
+        this.connectionTimeout = connectionTimeout;
+    }
+
+
+
+}
