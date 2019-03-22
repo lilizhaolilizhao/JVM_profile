34c391b7ee47f627ecfa9e7eeafaa663e812f4c5
lilizhaolilizhao <lilizhaolilizhao@163.com>
2019-03-22 15:13:04 +0800

    完善命令提示

--- agent/src/main/java/com/github/jvm/agent/shell/term/impl/RequestHandler.java
+++ agent/src/main/java/com/github/jvm/agent/shell/term/impl/RequestHandler.java
@@ -3,6 +3,7 @@ package com.github.jvm.agent.shell.term.impl;
 import com.github.jvm.agent.command.Command;
 import com.github.jvm.agent.handlers.Handler;
 import com.github.jvm.agent.shell.term.TermServer;
+import com.github.jvm.agent.shell.util.Helper;
 import com.github.jvm.agent.util.CommandParseUtil;
 import io.termd.core.function.Consumer;
 import io.termd.core.tty.TtyConnection;
@@ -26,8 +27,14 @@ public class RequestHandler implements Consumer<String> {
         term.setInReadline(false);
         Consumer<int[]> out = conn.stdoutHandler();
 
-        Command command = CommandParseUtil.parseCommand(line);
-        command.proecss(out);
+        if (line != null && !"".equals(line.trim())) {
+            try {
+                Command command = CommandParseUtil.parseCommand(line);
+                command.proecss(out);
+            } catch (Exception e) {
+                Helper.echoMsg(out, "命令格式异常." + "\n");
+            }
+        }
 
         term.readline(conn);
         lineHandler.handle(line);
